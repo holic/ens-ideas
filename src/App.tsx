277d1b7e19ed3cf40c72f383ad13998e2ca28c22
Kevin Ingersoll <kingersoll@gmail.com>
2021-12-19 18:35:40 -0800

    enter/submit also opens the link

--- src/App.tsx
+++ src/App.tsx
@@ -38,12 +38,19 @@ export const App = () => {
     return promise;
   }, [query]);
 
+  const url = `https://app.ens.domains/name/${encodeURIComponent(
+    `${query}.eth`
+  )}`;
+
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center bg-indigo-400">
-      <div className="w-2/3 flex flex-col flex-wrap gap-8 transition p-10 bg-white text-gray-600 rounded-xl shadow-2xl">
+      <form
+        action={url}
+        target="_blank"
+        className="w-2/3 flex flex-col flex-wrap gap-8 transition p-10 bg-white text-gray-600 rounded-xl shadow-2xl"
+      >
         <input
           className="outline-none border-4 border-gray-200 focus:border-blue-300 rounded-full px-6 py-3 text-3xl"
-          type="url"
           placeholder="Find a .eth domain"
           value={query}
           onChange={(event) => {
@@ -58,13 +65,14 @@ export const App = () => {
             );
           }}
           autoFocus
+          spellCheck={false}
+          autoCapitalize="off"
+          autoCorrect="off"
         />
         <div className="p-6 text-4xl font-bold">
           {query !== "" ? (
             <a
-              href={`https://app.ens.domains/name/${encodeURIComponent(
-                `${query}.eth`
-              )}`}
+              href={url}
               target="_blank"
               className={classNames(
                 "group flex flex-wrap gap-6 items-center justify-between",
@@ -82,7 +90,7 @@ export const App = () => {
             <>&nbsp;</>
           )}
         </div>
-      </div>
+      </form>
     </div>
   );
 };

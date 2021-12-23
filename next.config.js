c37a244790cda3de23907d67e91b3787a92b5165
Kevin Ingersoll <kingersoll@gmail.com>
2021-12-22 17:37:31 -0800

    use subdomain for api

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  rewrites: async () => ({
+    beforeFiles: [
+      {
+        has: [
+          {
+            type: "host",
+            value: "api.ensideas.com",
+          },
+        ],
+        source: "/:path*",
+        destination: "/api/:path*",
+      },
+    ],
+  }),
+  redirects: async () => [
+    {
+      has: [
+        {
+          type: "host",
+          value: "ensideas.com",
+        },
+      ],
+      source: "/api/:path*",
+      destination: "https://api.ensideas.com/:path*",
+      permanent: false,
+    },
+  ],
+};

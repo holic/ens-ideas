9e480a4e78f454125338795572c49657e6c03bb2
Kevin Ingersoll <kingersoll@gmail.com>
2021-12-22 18:14:14 -0800

    add api endpoint to resolve address to ens name/avatar

---
+++ src/pages/api/ens/resolve/[address].ts
@@ -0,0 +1,34 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { StaticJsonRpcProvider } from "@ethersproject/providers";
+
+const provider = new StaticJsonRpcProvider(
+  "https://eth-mainnet.alchemyapi.io/v2/5v4BuuWBFvvYHZoZZP5xFo2q1ldvABwj"
+);
+
+const firstParam = (param: string | string[]) => {
+  return Array.isArray(param) ? param[0] : param;
+};
+
+type Data =
+  | {
+      address: string;
+      name: string | null;
+      avatar: string | null;
+    }
+  | { address: string; error: string };
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const address = firstParam(req.query.address);
+
+  try {
+    const name = await provider.lookupAddress(address);
+    const avatar = name ? await provider.getAvatar(name) : null;
+    res.status(200).json({ address, name, avatar });
+  } catch (error: any) {
+    res.status(500).json({ address, error: error.message });
+  }
+}

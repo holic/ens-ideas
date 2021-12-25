af2469108e90bc299053212892f243cc94a53281
Kevin Ingersoll <kingersoll@gmail.com>
2021-12-25 11:03:38 -0800

    filter out expired domains past their redemption period

--- src/useRegistration.ts
+++ src/useRegistration.ts
@@ -1,10 +1,14 @@
 import { useQuery, gql } from "urql";
+import { DateTime } from "luxon";
 
 // TODO: figure out how to generate/extract types from this query
 
 const RegistrationQuery = gql`
-  query RegistrationQuery($name: String!) {
-    registrations(where: { labelName: $name }, first: 1) {
+  query RegistrationQuery($name: String!, $earliestExpiry: Int!) {
+    registrations(
+      where: { labelName: $name, expiryDate_gt: $earliestExpiry }
+      first: 1
+    ) {
       labelName
       registrationDate
       expiryDate
@@ -15,9 +19,15 @@ const RegistrationQuery = gql`
 export const useRegistration = (name: string) => {
   const isValid = name.length >= 3;
 
+  // Registrations subgraph includes expired domains, but we can filter them out
+  // with an `expiryDate` filter to account for the 90 day grace period.
+  const earliestExpiry = Math.floor(
+    DateTime.now().minus({ days: 90 }).toSeconds()
+  );
+
   const [result, reexecuteQuery] = useQuery({
     query: RegistrationQuery,
-    variables: { name },
+    variables: { name, earliestExpiry },
     pause: !isValid,
     requestPolicy: "network-only",
   });

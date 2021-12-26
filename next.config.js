ed7600e5ebf581aa3ab4613d6277d54da5d787c1
Kevin Ingersoll <kingersoll@gmail.com>
2021-12-26 11:12:08 -0800

    add cors headers to api

--- next.config.js
+++ next.config.js
@@ -15,4 +15,23 @@ module.exports = {
       },
     ],
   }),
+  headers: async () => [
+    {
+      has: [
+        {
+          type: "host",
+          value: "api.ensideas.com",
+        },
+      ],
+      source: "/:path*",
+      headers: [
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        {
+          key: "Access-Control-Allow-Methods",
+          value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+        },
+      ],
+    },
+  ],
 };

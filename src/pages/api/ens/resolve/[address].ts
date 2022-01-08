ba97c1911d7697f4dc5b69bb53c9216156b4a7fd
Kevin Ingersoll <kingersoll@gmail.com>
2022-01-07 22:39:47 -0800

    normalize address requests with redirect, add display name

--- src/pages/api/ens/resolve/[address].ts
+++ src/pages/api/ens/resolve/[address].ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { StaticJsonRpcProvider } from "@ethersproject/providers";
+import { getAddress } from "@ethersproject/address";
 
 // Cloudflare ETH node has a 1k req/minute limit
 const provider = new StaticJsonRpcProvider("https://cloudflare-eth.com");
@@ -13,6 +14,7 @@ type Data =
   | {
       address: string;
       name: string | null;
+      displayName: string;
       avatar: string | null;
     }
   | { address: string; error: string };
@@ -21,10 +23,19 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const address = firstParam(req.query.address);
+  const inputAddress = firstParam(req.query.address);
+  const address = getAddress(inputAddress.toLowerCase());
+
+  if (address !== inputAddress) {
+    return res.redirect(308, `/api/ens/resolve/${address}`);
+  }
 
   try {
     const name = await provider.lookupAddress(address);
+    const displayName =
+      name ||
+      address.replace(/^(0x[0-9A-F]{3})[0-9A-F]+([0-9A-F]{4})$/i, "$1…$2");
+
     const avatar = name ? await provider.getAvatar(name) : null;
 
     res
@@ -33,7 +44,7 @@ export default async function handler(
         "CDN-Cache-Control",
         `s-maxage=${60 * 60 * 24}, stale-while-revalidate`
       )
-      .json({ address, name, avatar });
+      .json({ address, name, displayName, avatar });
   } catch (error: any) {
     res.status(500).json({ address, error: error.message });
   }

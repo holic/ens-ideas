65a5e677ae58f39419c1bfbb8e1af031537ece14
Kevin Ingersoll <kingersoll@gmail.com>
2023-04-03 17:39:42 -0700

    split-test viem (#5)
    * add viem
    
    * switch ens resolving to viem
    
    * Revert "switch ens resolving to viem"
    
    This reverts commit 20d90a486409086b4ed9d447879cd253aaa2a545.
    
    * refactor
    
    * more refactoring
    
    * split test ethers/viem

--- src/pages/api/ens/resolve/[address].ts
+++ src/pages/api/ens/resolve/[address].ts
@@ -1,10 +1,14 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { StaticJsonRpcProvider } from "@ethersproject/providers";
-import { getAddress, isAddress } from "@ethersproject/address";
-import url from "url";
+import { getAddress, isAddress, createPublicClient, http, Address } from "viem";
+import { mainnet } from "viem/chains";
+import { normalize } from "viem/ens";
 
 const provider = new StaticJsonRpcProvider(process.env.ETHEREUM_RPC_URL);
+const publicClient = createPublicClient({
+  chain: mainnet,
+  transport: http(process.env.ETHEREUM_RPC_URL),
+});
 
 const firstParam = (param: string | string[]) => {
   return Array.isArray(param) ? param[0] : param;
@@ -19,7 +23,7 @@ const resolve = (from: string, to: string) => {
   return resolvedUrl.toString();
 };
 
-type Data = {
+type ResponseData = {
   address: string | null;
   name: string | null;
   displayName: string;
@@ -27,10 +31,72 @@ type Data = {
   error?: string;
 };
 
+const getEnsName = async ({
+  address,
+}: {
+  address: Address;
+}): Promise<string | null> => {
+  const ethersPromise = provider.lookupAddress(address);
+  Promise.allSettled([
+    ethersPromise,
+    publicClient.getEnsName({ address }),
+  ]).then(([ethersResult, viemResult]) => {
+    if (JSON.stringify(ethersResult) !== JSON.stringify(viemResult)) {
+      console.warn("Inconsistent results for getEnsName:", {
+        address,
+        ethersResult,
+        viemResult,
+      });
+    }
+  });
+  return ethersPromise;
+};
+
+const getEnsAddress = async ({
+  name,
+}: {
+  name: string;
+}): Promise<string | null> => {
+  const ethersPromise = provider.resolveName(name);
+  Promise.allSettled([
+    ethersPromise,
+    publicClient.getEnsAddress({ name: normalize(name) }),
+  ]).then(([ethersResult, viemResult]) => {
+    if (JSON.stringify(ethersResult) !== JSON.stringify(viemResult)) {
+      console.warn("Inconsistent results for getEnsAddress:", {
+        name,
+        ethersResult,
+        viemResult,
+      });
+    }
+  });
+  return ethersPromise;
+};
+
+const getEnsAvatar = async ({
+  name,
+}: {
+  name: string;
+}): Promise<string | null> => {
+  const ethersPromise = provider.getAvatar(name);
+  Promise.allSettled([
+    ethersPromise,
+    publicClient.getEnsAvatar({ name: normalize(name) }),
+  ]).then(([ethersResult, viemResult]) => {
+    if (JSON.stringify(ethersResult) !== JSON.stringify(viemResult)) {
+      console.warn("Inconsistent results for getEnsAvatar:", {
+        name,
+        ethersResult,
+        viemResult,
+      });
+    }
+  });
+  return ethersPromise;
+};
+
 const resolveAddress = async (
-  lowercaseAddress: string,
-  res: NextApiResponse<Data>
-) => {
+  lowercaseAddress: string
+): Promise<ResponseData> => {
   const address = getAddress(lowercaseAddress);
   let displayName = address.replace(
     /^(0x[0-9A-F]{3})[0-9A-F]+([0-9A-F]{4})$/i,
@@ -38,59 +104,45 @@ const resolveAddress = async (
   );
 
   try {
-    const name = await provider.lookupAddress(address);
+    const name = await getEnsName({ address });
     if (name) {
       displayName = name;
     }
-
-    const avatar = name ? await provider.getAvatar(name) : null;
-
-    res
-      .status(200)
-      .setHeader(
-        "CDN-Cache-Control",
-        `s-maxage=${60 * 60 * 24}, stale-while-revalidate`
-      )
-      .json({ address, name, displayName, avatar });
+    const avatar = name ? await getEnsAvatar({ name }) : null;
+    return { address, name, displayName, avatar };
   } catch (error: any) {
-    res.status(500).json({
+    return {
       address,
       name: null,
       displayName,
       avatar: null,
       error: error.message,
-    });
+    };
   }
 };
 
-const resolveName = async (name: string, res: NextApiResponse<Data>) => {
+const resolveName = async (name: string): Promise<ResponseData> => {
   const displayName = name;
   try {
     const [address, avatar] = await Promise.all([
-      provider.resolveName(name),
-      provider.getAvatar(name),
+      getEnsAddress({ name }),
+      getEnsAvatar({ name }),
     ]);
-    res
-      .status(200)
-      .setHeader(
-        "CDN-Cache-Control",
-        `s-maxage=${60 * 60 * 24}, stale-while-revalidate`
-      )
-      .json({ address, name, displayName, avatar });
+    return { address, name, displayName, avatar };
   } catch (error: any) {
-    res.status(500).json({
+    return {
       address: null,
       name,
       displayName,
       avatar: null,
       error: error.message,
-    });
+    };
   }
 };
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<ResponseData>
 ) {
   const inputAddress = firstParam(req.query.address);
   const lowercaseAddress = inputAddress.toLowerCase();
@@ -99,7 +151,19 @@ export default async function handler(
     return res.redirect(307, resolve(req.url!, lowercaseAddress));
   }
 
-  return isAddress(lowercaseAddress)
-    ? resolveAddress(lowercaseAddress, res)
-    : resolveName(lowercaseAddress, res);
+  const data = isAddress(lowercaseAddress)
+    ? await resolveAddress(lowercaseAddress)
+    : await resolveName(lowercaseAddress);
+
+  if (data.error) {
+    return res.status(500).json(data);
+  }
+
+  return res
+    .status(200)
+    .setHeader(
+      "CDN-Cache-Control",
+      `s-maxage=${60 * 60 * 24}, stale-while-revalidate`
+    )
+    .json(data);
 }
